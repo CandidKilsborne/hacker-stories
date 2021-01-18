c4b91fb3353070226616f0ea80a89f43147767ac
Aaron Piotrowski <a.paul.piotrowski@gmail.com>
2021-01-17 20:49:12 -0500

    go over inline handlers

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const useSemiPersistentState = (key, initialState) => {
 };
 
 const App = () => {
-  const stories = [
+  const initialStories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -33,6 +33,15 @@ const App = () => {
   ];
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  };
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -57,22 +66,29 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </>
   );
 };
 
-const List = ({ list }) =>
-  list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map(({ objectID, ...item }) => (
+    <Item key={objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
 
-const Item = ({ url, title, author, num_comments, points }) => (
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type='button' onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
   </div>
 );
 
