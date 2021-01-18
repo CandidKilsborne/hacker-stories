33bbf4d968d1cffc68e8466ad0b32397ba6f2750
Aaron Piotrowski <a.paul.piotrowski@gmail.com>
2021-01-17 20:29:13 -0500

    Setup focus

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -15,30 +15,30 @@ const useSemiPersistentState = (key, initialState) => {
 const App = () => {
   const stories = [
     {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
       objectID: 1,
     },
   ];
 
-  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
-  const handleSearch = (event) => {
+  const handleSearch = event => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter((story) =>
+  const searchedStories = stories.filter(story =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -47,8 +47,9 @@ const App = () => {
       <h1>My Hacker Stories</h1>
 
       <InputWithLabel
-        id="search"
+        id='search'
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
       >
         <strong>Search:</strong>
@@ -79,14 +80,32 @@ const InputWithLabel = ({
   id,
   children,
   value,
-  type = "text",
+  type = 'text',
+  isFocused,
   onInputChange,
-}) => (
-  <>
-    <label htmlFor={id}>{children}</label>
-    &nbsp;
-    <input id={id} type={type} value={value} onChange={onInputChange} />
-  </>
-);
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
 
 export default App;
