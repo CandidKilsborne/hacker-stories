55ce16ad99f7b7693e27e6c8a183db99ce38f8aa
Aaron Piotrowski <aaron@trakid.com>
2021-02-16 10:34:52 -0500

    added conditional rendering for async calls

--- src/App.js
+++ src/App.js
@@ -33,19 +33,24 @@ const App = () => {
   ];
 
   const getAsyncStories = () =>
-    new Promise(
-      resolve =>
-        setTimeout(() => resolve({ data: { stories: initialStories } })),
-      2000
+    new Promise(resolve =>
+      setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
     );
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
-    getAsyncStories().then(result => {
-      setStories(result.data.stories);
-    });
+    setIsLoading(true);
+
+    getAsyncStories()
+      .then(result => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = item => {
@@ -79,7 +84,13 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong...</p>}
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </>
   );
 };
